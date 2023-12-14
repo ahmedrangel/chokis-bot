1393dc5029455d1ed1f005e38b86d60a895f50f2
Ahmed Rangel <ahmedrangel@outlook.com>
2023-12-14 06:45:10 -0500

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -2,13 +2,33 @@
  * Cloudflare worker.
  */
 import { Router } from "itty-router";
-import { verifyKey } from "./lib/discord.js";
+import { API, verifyKey } from "./lib/discord.js";
 import { commandsHandler } from "./handler.js";
 
 const router = Router();
 
 router.get("/", (req, env) => {
   return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
+
+router.get("/giveaways/participants/:guildId", async (req, env) => {
+  const { guildId } = req.params;
+  const data = await env.CHOKISDB.prepare(`SELECT * FROM giveaways WHERE guildId = '${guildId}'`).all();
+  const guildPreview = await fetch(`${API.BASE}/guilds/${guildId}/preview`, {
+    headers: {
+      "Authorization" : "Bot " + env.DISCORD_TOKEN
+    }
+  });
+  const guildData = await guildPreview.json();
+  const guildResults = {
+    name: guildData?.name,
+    icon: guildData?.icon
+  };
+  const obj = {
+    list: data.results,
+    guild: guildResults
+  };
+  return new Response(JSON.stringify(obj));
 });
 
 // Commands Handler
