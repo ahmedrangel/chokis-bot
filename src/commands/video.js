7a6e7ec110979356c3c2b69d8c1b34100112d0a3
Ahmed Rangel <ahmedrangel@outlook.com>
2024-04-04 05:29:35 -0500

    chore: adjust

--- src/commands/video.js
+++ src/commands/video.js
@@ -6,7 +6,6 @@ import { ButtonStyleTypes, MessageComponentTypes } from "discord-interactions";
 
 export const video = (getValue, env, context, request_data) => {
   const { guild_id, token } = request_data;
-  const bot_name = CONSTANTS.BOT.toLowerCase();
   const followUpRequest = async () => {
     let mensaje, emoji;
     let embeds = [], files = [], button = [], components = [];
@@ -54,7 +53,7 @@ export const video = (getValue, env, context, request_data) => {
         const maxSize = guild.premium_tier >= 3 ? 100000000 : (guild.premium_tier === 2 ? 50000000 : 25000000);
         if (fileSize > 100 && fileSize < maxSize) {
           const encodedScrappedUrl = encodeURIComponent(url_scrapped);
-          const upload = await fetch(`${env.EXT_WORKER_AHMED}/put-r2?video_url=${encodedScrappedUrl}&bot_name=${bot_name}`);
+          const upload = await fetch(`${env.EXT_WORKER_AHMED}/put-r2?video_url=${encodedScrappedUrl}&bot_name=${CONSTANTS.BOT}`);
           const url_uploaded = await upload.text();
           const urlId = obtenerIDDesdeURL(url_uploaded);
           files.push({
